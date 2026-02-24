dbeb9268bfa54c4cc6df0094dee75b3bbfd0d326
seiya_iwabuchi <0123ook@gmail.com>
2026-02-24 21:52:03 +0900

    feat: astro.config.mjsのサイトURLとベースパスを更新 fix: Features.astroの画像パスを相対パスに修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://seiyaiwabuchi.github.io/',
+  site: 'https://seiyaiwabuchi.github.io/pet-kanri-app-lp/',
+  base: '/pet-kanri-app-lp/',
 });
